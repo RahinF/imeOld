8d593c09552c13af9f2af81ca2d223e7dc1e821c
RahinF <s3297352@student.rmit.edu.au>
2021-05-05 11:54:55 +1000

    created message input field

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+
   return (
     <div className="App">
-      
+      <h1>iMe</h1>
+
+      {/* Message Area */}
+
+      {/* Input Field */}
+      <input
+        type="text"
+        placeholder="Enter message..."
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+
+      {/* Input Button */}
+      <button>Send message</button>
     </div>
   );
 }
