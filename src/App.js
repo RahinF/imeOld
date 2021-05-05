2671e9f0a6a4c783d11bdd590c093821a7102e86
RahinF <s3297352@student.rmit.edu.au>
2021-05-05 12:18:46 +1000

    added ability to send and display messages

--- src/App.js
+++ src/App.js
@@ -3,23 +3,41 @@ import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = (event) => {
+
+    // stops refreshing when form is submitted
+    event.preventDefault();
+    
+    // append to message array
+    setMessages([...messages, input]);
+
+    // clear input field
+    setInput("");
+  };
 
   return (
     <div className="App">
       <h1>iMe</h1>
 
       {/* Message Area */}
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
 
-      {/* Input Field */}
-      <input
-        type="text"
-        placeholder="Enter message..."
-        value={input}
-        onChange={(event) => setInput(event.target.value)}
-      />
+      <form>
+        <input
+          type="text"
+          placeholder="Enter message..."
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
 
-      {/* Input Button */}
-      <button>Send message</button>
+        <button type="submit" onClick={sendMessage}>
+          Send message
+        </button>
+      </form>
     </div>
   );
 }
