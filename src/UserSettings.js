d57870320beddc60bb0c8019007fa30b8be8141f
RahinF <s3297352@student.rmit.edu.au>
2021-06-06 12:58:49 +1000

    added layout to user setting

--- src/UserSettings.js
+++ src/UserSettings.js
@@ -1,7 +1,23 @@
-import { Avatar, IconButton } from "@material-ui/core";
+import {
+  Avatar,
+  Badge,
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  IconButton,
+  TextField,
+} from "@material-ui/core";
 import { useEffect, useRef, useState } from "react";
 import { database, storage } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import SettingsIcon from "@material-ui/icons/Settings";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
 
 function UserSettings() {
   const inputFile = useRef(null);
@@ -9,18 +25,17 @@ function UserSettings() {
   const [displayName, setDisplayName] = useState(user.displayName);
   const [comment, setComment] = useState(user.comment);
 
-  const submitDisplayNameChange = () => {
+  const submitUserInfoChange = () => {
     database
       .collection("users")
       .doc(user.uid)
-      .update({ displayName: displayName });
-  };
+      .update({ displayName: displayName, comment: comment });
 
-  const submitCommentChange = () => {
-    database.collection("users").doc(user.uid).update({ comment: comment });
+    closeDialogBox();
   };
 
   useEffect(() => {
+    //  pull user data from db if changed
     database
       .collection("users")
       .doc(user.uid)
@@ -33,10 +48,12 @@ function UserSettings() {
   }, [user.uid, dispatch]);
 
   const submitAvatarChange = async (event) => {
-    if (!event.target.files[0]) {
+    const file = event.target.files[0];
+
+    if (!file) {
       return;
     }
-    const file = event.target.files[0];
+
     const storageRef = storage.ref();
     const fileRef = storageRef.child(`avatar`).child(`${user.uid}.jpg`);
 
@@ -55,39 +72,116 @@ function UserSettings() {
     inputFile.current.click();
   };
 
+  //
+  const [open, setOpen] = useState(false);
+
+  const openDialogBox = () => {
+    setOpen(true);
+  };
+
+  const closeDialogBox = () => {
+    setOpen(false);
+  };
+  //
+
   return (
     <div>
-      <input
-        type="text"
-        placeholder="name"
-        value={displayName}
-        onChange={(event) => setDisplayName(event.target.value)}
-      />
-      <button onClick={submitDisplayNameChange}>save name change</button>
-
-      <input
-        type="text"
-        placeholder="comment"
-        value={comment}
-        onChange={(event) => setComment(event.target.value)}
-      />
-      <button onClick={submitCommentChange}>save comment change</button>
-
-      {/* Profile picture change */}
-      <IconButton color="primary" component="span">
-        <Avatar
-          src={user.avatar || "http://via.placeholder.com/300"}
-          alt={`${user.displayName}'s avatar`}
-          onClick={openFileUpload}
+      <Card style={{ width: "400px" }}>
+        <CardHeader
+          avatar={
+            <Avatar
+              src={user.avatar || "http://via.placeholder.com/300"}
+              alt={`${user.displayName}'s avatar`}
+            />
+          }
+          action={
+            <IconButton
+              color="primary"
+              onClick={openDialogBox}
+              style={{ marginTop: "0" }}
+            >
+              <SettingsIcon />
+            </IconButton>
+          }
+          title={user.displayName}
+          subheader={user.comment}
         />
-      </IconButton>
+      </Card>
 
-      <input
-        type="file"
-        ref={inputFile}
-        style={{ display: "none" }}
-        onChange={submitAvatarChange}
-      />
+      <Dialog open={open} onClose={closeDialogBox}>
+        <DialogTitle>User Settings</DialogTitle>
+        <div>
+          <Grid container justify="center">
+            <IconButton
+              color="primary"
+              component="span"
+              onClick={openFileUpload}
+            >
+              <Badge
+                overlap="circle"
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                badgeContent={<AddAPhotoIcon color="secondary"/>}
+                
+                
+              >
+                <Avatar
+                  src={user.avatar || "http://via.placeholder.com/300"}
+                  alt={`${user.displayName}'s avatar`}
+                  style={{ height: "100px", width: "100px" }}
+                />
+              </Badge>
+            </IconButton>
+
+            <input
+              type="file"
+              accept="image/png, image/gif, image/jpeg"
+              ref={inputFile}
+              style={{ display: "none" }}
+              onChange={submitAvatarChange}
+            />
+          </Grid>
+
+          <Box m={4}>
+            <TextField
+              type="text"
+              label="Display Name"
+              value={displayName}
+              onChange={(event) => setDisplayName(event.target.value)}
+              variant="outlined"
+              fullWidth
+            />
+          </Box>
+
+          <Box m={4}>
+            <TextField
+              type="text"
+              label="Comment"
+              value={comment}
+              onChange={(event) => setComment(event.target.value)}
+              variant="outlined"
+              fullWidth
+            />
+          </Box>
+        </div>
+
+        <DialogActions>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={submitUserInfoChange}
+            disabled={!displayName}
+          >
+            Save
+          </Button>
+
+          <Button onClick={closeDialogBox} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
